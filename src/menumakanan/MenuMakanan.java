4e853f43fd7ff16157af6b5a0a589b53b7d69788
Arifin <walangsangit5632@gmail.com>
2025-09-17 00:41:07 +0700

    NasiGoreng

--- src/menumakanan/MenuMakanan.java
+++ src/menumakanan/MenuMakanan.java
@@ -1,7 +1,35 @@
 package menumakanan;
+
 public class MenuMakanan {
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        /*- Nama: Nasi Goreng Kampung
+          - Jenis: Makanan Indonesia
+          - Harga: 20.000
+          - Kalori: 500 kkal
+          - Topping: Telur ceplok, kerupuk*/
+        System.out.println("=========================");
+        System.out.println("       Menu Makanan      ");
+        System.out.println("=========================");
+        NasiGoreng nasgor = new NasiGoreng();
+        
+        nasgor.setnama("Nasi Goreng Kampung");
+        System.out.println("Nama    : "+nasgor.getnama());
+        
+        nasgor.setjenis("Makanan Indonesia");
+        System.out.println("Jenis   : "+nasgor.getjenis());
+        
+        nasgor.setharga("20.000");
+        System.out.println("Harga   : "+nasgor.getharga());
+        
+        nasgor.setkalori("500 kkal");
+        System.out.println("Kalori  : "+nasgor.getkalori());
+        
+        nasgor.settopping("Telur Ceplok dan Kerupuk");
+        System.out.println("Topping : "+nasgor.gettopping());
+        
+        
     }
-    
+  
 }
--- src/menumakanan/NasiGoreng.java
+++ src/menumakanan/NasiGoreng.java
@@ -1,4 +1,40 @@
 package menumakanan;
+
 public class NasiGoreng {
-    
+    String nama, jenis, harga, kalori, topping;
+   
+    public void setnama(String namaP){
+        this.nama = namaP;
+    }
+    public String getnama(){
+        return this.nama;
+    }
+    public void setjenis(String jenisP){
+        this.jenis = jenisP;
+    }
+    public String getjenis(){
+        return this.jenis;
+    }  
+    public void setharga(String hargaP){
+        this.harga = hargaP;
+    }
+    public String getharga(){
+        return this.harga;
+    }
+    public void setkalori(String kaloriP){
+        this.kalori = kaloriP;
+    }
+    public String getkalori(){
+        return this.kalori;
+    }
+    public void settopping(String toppingP){
+        this.topping = toppingP;
+    }
+    public String gettopping(){
+        return this.topping;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+    }
 }
