2c6adece72c88ecb50d1c7e402aadd86cd10446f
ramunir1306 <munirabdul0605@gmail.com>
2025-09-17 00:44:27 +0700

    tes3

--- src/menumakanan/MenuMakanan.java
+++ src/menumakanan/MenuMakanan.java
@@ -2,6 +2,21 @@ package menumakanan;
 public class MenuMakanan {
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("==============================");
+        System.out.println("     MENU MAKANAN PILIHAN     ");
+        System.out.println("==============================");
+        Ramen D = new Ramen();
+        D.setnama("Ramen Shoyu");
+        System.out.println("Nama    :"+D.getnama());
+        D.setjenis("Makanan Jepang");
+        System.out.println("Nama    :"+D.getjenis());
+        D.setharga("Rp 50.000");
+        System.out.println("Nama    :"+D.getharga());
+        D.setkalori("900 kkal");
+        System.out.println("Nama    :"+D.getkalori());
+        D.setlevel_pedas("Sedang");
+        System.out.println("Nama    :"+D.getlevel_pedas());
+        System.out.println("==============================");
     }
     
 }
