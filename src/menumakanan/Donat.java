6a4d78231bf17532d4bf278eac32151668f96ea3
Nasrul809 <hadinasrul809@gmail.com>
2025-09-17 07:55:32 +0700

    donat selesai

--- src/menumakanan/Donat.java
+++ src/menumakanan/Donat.java
@@ -1,4 +1,41 @@
 package menumakanan;
+
 public class Donat {
     
+    String nama, jenis, harga, kalori, topping;
+    
+    public void setnama(String namaD){
+        this.nama = namaD;
+    }
+    public String getnama(){
+        return this.nama;
+    }
+    
+    public void setjenis(String jenisD){
+        this.nama = jenisD;
+    }
+    public String getjenis(){
+        return this.jenis;
+    }
+    
+    public void setharga(String hargaD){
+        this.harga = hargaD;
+    }
+    public String getharga(){
+        return this.harga;
+    }
+    
+    public void setkalori(String kaloriD){
+        this.nama = kaloriD;
+    }
+    public String getkalori(){
+        return this.kalori;
+    }
+    
+    public void settopping(String toppingD){
+        this.nama = toppingD;
+    }
+    public String gettoppig(){
+        return this.topping;
+    }
 }
--- src/menumakanan/MenuMakanan.java
+++ src/menumakanan/MenuMakanan.java
@@ -1,7 +1,23 @@
 package menumakanan;
+
 public class MenuMakanan {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("=====================================");
+        System.out.println("         MENU MAKANAN PILIHAN        ");
+        System.out.println("=====================================");
+        Donat d = new Donat();
+        d.setnama("Donat Cokelat");
+        System.out.println("Nama = "+ d.getnama());
+        d.setjenis("Dessert");
+        System.out.println("Jenis = "+d.getjenis());
+        d.setharga("Rp 10.000");
+        System.out.println("Harga = "+d.getharga());
+        d.setkalori("300 kkal");
+        System.out.println("Kalori = "+d.getkalori());
+        d.settopping("Cokelat meses, Gula halus");
+        System.out.println("Topping = "+d.gettoppig());
+        System.out.println("=====================================");
     }
     
 }
