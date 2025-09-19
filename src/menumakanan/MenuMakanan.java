f695b00ab02906507d0bb5d821e6738584f7b644
miraatuzzakya@gmail.com <Lenovo@LAPTOP-C1OS4GKM>
2025-09-19 21:28:22 +0700

    mira saladbuah

--- src/menumakanan/MenuMakanan.java
+++ src/menumakanan/MenuMakanan.java
@@ -1,7 +1,24 @@
 package menumakanan;
 public class MenuMakanan {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("======================================"); 
+        System.out.println("      MENU MAKANAN PILIHAN     ");
+        System.out.println("======================================");
+        SaladBuah P = new SaladBuah();
+        P.setnama("Salad Buah");
+        System.out.println("Nama      : " + P.getnama());
+        P.setjenis("Makanan Sehat");
+        System.out.println("Jenis     : " + P.getjenis());
+        P.setharga("15000");
+        System.out.println("Harga     : " + P.getharga());
+        P.setkalori("200 kkal");
+        System.out.println("Kalori    : " + P.getkalori());
+        P.setbuah("Anggur, Semangka, Kiwi, Melon, Jeruk, Strawbery");
+        System.out.println("Buah      : " + P.getbuah());
+        P.settoping("Keju, Coklat, Keju & Coklat");
+        System.out.println("Toping    : " + P.gettoping());
+        System.out.println("======================================");
     }
     
 }
--- src/menumakanan/SaladBuah.java
+++ src/menumakanan/SaladBuah.java
@@ -1,4 +1,40 @@
 package menumakanan;
 public class SaladBuah {
-    
+    String nama, jenis, harga, kalori, buah, toping;
+        public void setnama(String namaP) {
+        this.nama = namaP;
+    }
+    public String getnama() {
+        return this.nama;
+    } 
+    public void setjenis(String jenisP) {
+        this.jenis = jenisP;
+    }
+    public String getjenis() {
+        return this.jenis;
+    } 
+    public void setharga(String hargaP) {
+        this.harga = hargaP;
+    }
+    public String getharga() {
+        return this.harga;
+    } 
+    public void setkalori(String kaloriP) {
+        this.kalori = kaloriP;
+    }
+    public String getkalori() {
+        return this.kalori;
+    } 
+    public void setbuah(String buahP) {
+        this.buah = buahP;
+    }
+    public String getbuah() {
+        return this.buah;
+    } 
+     public void settoping(String topingP) {
+        this.toping = topingP;
+    }
+    public String gettoping() {
+        return this.toping;
+    }
 }
